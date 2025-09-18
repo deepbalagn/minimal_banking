ddb16fb2109af0e8fb5d71cf23f7762d7773f7a9
Deep Bala <deep.gnwebsoft@gmail.com>
2025-09-18 11:47:24 +0530

    before restructure

--- src/pages/banking/LeftSide.tsx
+++ src/pages/banking/LeftSide.tsx
@@ -1,29 +1,17 @@
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import { Box,  Grid, Paper, Typography } from "@mui/material";
 import {
-  Add,
-  AddAlarm,
-  ArrowDownward,
-  ArrowUpward,
+  ArrowRight,
   AttachMoney,
   Circle,
-  InfoOutlineRounded,
 } from "@mui/icons-material";
 import { Icon } from "@iconify/react";
-import {
-  LineChart,
-  lineElementClasses,
-  markElementClasses,
-} from "@mui/x-charts/LineChart";
+
 import { BarChart } from "@mui/x-charts/BarChart";
 import { useState } from "react";
 import CustomizedMenus from "../../customizedComponents/StyledMenu";
 import { PieChart } from "@mui/x-charts";
+import IncomeExpenensePaper from "./components/incomeExpencePaper";
 
-
-const margin = { right: 24 };
-const incomeData = [5, 31, 33, 50, 99, 76, 72, 76, 89];
-const expenseData = [5, 31, 33, 50, 99, 76, 72, 76, 89];
-const xLabels = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep"];
 
 const barIncomeData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const barSavingsData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
@@ -33,225 +21,10 @@ const monthlyBarxLabels = ["2018", "2019", "2020", "2021", "2022", "2023"];
 const weeklyBarxLabels = ["2018", "2019", "2020", "2021", "2022", "2023"];
 
 export default function RightSide() {
-  const [isIncomeGraph, setIsIncomeGraph] = useState(true);
   const [selectedBarChart] = useState("yearly");
-  //   const [selectedBarChart, setSelectedBarChart] = useState('yearly');
   return (
     <>
-      <Paper sx={{ p: 3, mb: 2, borderRadius: 5 }} elevation={5}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <Typography variant="body1">Total Balance </Typography>{" "}
-              <InfoOutlineRounded sx={{ fontSize: 16 }} />
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <AttachMoney sx={{ fontWeight: "bold" }} />
-              <Typography variant="h5" fontWeight={"bold"}>
-                49,990
-              </Typography>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 2,
-              alignItems: "start",
-            }}
-          >
-            <Button
-              size="small"
-              variant="contained"
-              color="inherit"
-              href="/dashboard/tour/new"
-              disableElevation
-              startIcon={<ArrowUpward />}
-            >
-              Send
-            </Button>
-            <Button
-              size="small"
-              variant="contained"
-              color="inherit"
-              href="/dashboard/tour/new"
-              disableElevation
-              startIcon={<Add />}
-            >
-              Add Card
-            </Button>
-            <Button
-              size="small"
-              variant="contained"
-              color="inherit"
-              href="/dashboard/tour/new"
-              disableElevation
-              startIcon={<ArrowDownward />}
-            >
-              Request
-            </Button>
-          </Box>
-        </Box>
-        <Paper
-          sx={{
-            p: 3,
-            m: 1,
-            backgroundColor: "#F4F6F8",
-          }}
-        >
-          <Grid container>
-            <Grid size={6}>
-              <Button
-                color="inherit"
-                fullWidth
-                onClick={() => {
-                  setIsIncomeGraph(true);
-                }}
-              >
-                <Box sx={{ display: "flex", flexDirection: "row" }}>
-                  <Icon
-                    icon="eva:diagonal-arrow-left-down-fill"
-                    width="40"
-                    height="40"
-                  />
-                  <Box paddingLeft={3}>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                      }}
-                    >
-                      <Typography variant="body1">Income </Typography>{" "}
-                      <InfoOutlineRounded sx={{ fontSize: 16 }} />
-                    </Box>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                      }}
-                    >
-                      <AttachMoney sx={{ fontWeight: "bold" }} />
-                      <Typography variant="h5" fontWeight={"bold"}>
-                        9,990
-                      </Typography>
-                    </Box>
-                  </Box>
-                </Box>
-              </Button>
-            </Grid>
-            <Grid size={6}>
-              <Button
-                color="inherit"
-                fullWidth
-                onClick={() => {
-                  setIsIncomeGraph(false);
-                }}
-              >
-                <Icon
-                  icon="eva:diagonal-arrow-right-up-fill"
-                  width="40"
-                  height="40"
-                />
-                <Box paddingLeft={3}>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Typography variant="body1">Expenses </Typography>{" "}
-                    <InfoOutlineRounded sx={{ fontSize: 16 }} />
-                  </Box>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "center",
-                    }}
-                  >
-                    <AttachMoney sx={{ fontWeight: "bold" }} />
-                    <Typography variant="h5" fontWeight={"bold"}>
-                      1989
-                    </Typography>
-                  </Box>
-                </Box>
-              </Button>
-            </Grid>
-          </Grid>
-        </Paper>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          {isIncomeGraph ? (
-            <LineChart
-              height={300}
-              series={[{ data: incomeData, label: "income" }]}
-              xAxis={[{ scaleType: "point", data: xLabels }]}
-              yAxis={[{ width: 50 }]}
-              margin={margin}
-              sx={{
-                [`& .${lineElementClasses.root}`]: {
-                  stroke: "#1cca56ff",
-                  strokeWidth: 2,
-                },
-                [`& .${markElementClasses.root}`]: {
-                  stroke: "#07f3c0ff",
-                  r: 4, // Modify the circle radius
-                  fill: "#fff",
-                  strokeWidth: 2,
-                },
-              }}
-              disableAxisListener
-            />
-          ) : (
-            <LineChart
-              height={300}
-              series={[{ data: expenseData, label: "expense" }]}
-              xAxis={[{ scaleType: "point", data: xLabels }]}
-              yAxis={[{ width: 50 }]}
-              margin={margin}
-              sx={{
-                [`& .${lineElementClasses.root}`]: {
-                  stroke: "#c2163bff",
-                  strokeWidth: 2,
-                },
-                [`& .${markElementClasses.root}`]: {
-                  stroke: "#f80021ff",
-                  r: 4, // Modify the circle radius
-                  fill: "#fff",
-                  strokeWidth: 2,
-                },
-              }}
-              disableAxisListener
-            />
-          )}
-        </Box>
-      </Paper>
+      <IncomeExpenensePaper />
 
       <Paper sx={{ p: 3, mb: 2, borderRadius: 5 }} elevation={5}>
         <Box
@@ -406,12 +179,13 @@ export default function RightSide() {
         </Box>
       </Paper>
 
-      <Paper sx={{ p: 3, mb: 2, borderRadius: 5 }} elevation={5}>
+      <Paper sx={{ mb: 2, borderRadius: 5 }} elevation={5}>
         <Box
           sx={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
+            p: 3,
           }}
         >
           <Box>
@@ -446,18 +220,23 @@ export default function RightSide() {
             height={200}
             hideLegend={true}
           />
-          <Box width={'50%'}>
-            <Grid container>
+          <Box width={"50%"}>
+            <Grid container spacing={2}>
               <Grid size={6}>
                 <Box
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <Icon icon="mingcute:game-2-fill" width="24" height="24" color="purple" />
+                  <Icon
+                    icon="mingcute:game-2-fill"
+                    width="20"
+                    height="20"
+                    color="purple"
+                  />
                   <Typography>Entertainment ($22)</Typography>
                 </Box>
               </Grid>
@@ -466,11 +245,16 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <Icon icon="bi:fuel-pump-fill" width="16" height="16" color="red"/>
+                  <Icon
+                    icon="bi:fuel-pump-fill"
+                    width="20"
+                    height="20"
+                    color="red"
+                  />
                   <Typography>Fuel ($18)</Typography>
                 </Box>
               </Grid>
@@ -479,11 +263,16 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <Icon icon="ic:baseline-fastfood" width="24" height="24" color="green"/>
+                  <Icon
+                    icon="ic:baseline-fastfood"
+                    width="20"
+                    height="20"
+                    color="green"
+                  />
                   <Typography>Fast Food ($16)</Typography>
                 </Box>
               </Grid>
@@ -492,11 +281,16 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <Icon icon="carbon:cafe" width="32" height="32" color="yellow"/>
+                  <Icon
+                    icon="carbon:cafe"
+                    width="20"
+                    height="20"
+                    color="yellow"
+                  />
                   <Typography>Cafe ($17)</Typography>
                 </Box>
               </Grid>
@@ -505,11 +299,16 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <Icon icon="mynaui:speaker-solid" width="24" height="24" color="blue"/>
+                  <Icon
+                    icon="mynaui:speaker-solid"
+                    width="20"
+                    height="20"
+                    color="blue"
+                  />
                   <Typography>Connection ($14)</Typography>
                 </Box>
               </Grid>
@@ -518,11 +317,16 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <Icon icon="streamline-pixel:health-drug-medicine-bag-aid-1" width="32" height="32" color="blue" />
+                  <Icon
+                    icon="streamline-pixel:health-drug-medicine-bag-aid-1"
+                    width="20"
+                    height="20"
+                    color="blue"
+                  />
                   <Typography>Health Care ($22)</Typography>
                 </Box>
               </Grid>
@@ -531,12 +335,17 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <AddAlarm />
-                  <Typography>BALA ($22)</Typography>
+                  <Icon
+                    icon="material-symbols:fitness-center-rounded"
+                    width="20"
+                    height="20"
+                    color="green"
+                  />
+                  <Typography>Fitness ($10)</Typography>
                 </Box>
               </Grid>
               <Grid size={6}>
@@ -544,15 +353,99 @@ export default function RightSide() {
                   sx={{
                     display: "flex",
                     flexDirection: "row",
-                    alignContent: "center",
+                    alignItems: "center",
                     gap: 1,
                   }}
                 >
-                  <AddAlarm />
-                  <Typography>BALA ($22)</Typography>
+                  <Icon
+                    icon="fontisto:shopping-basket"
+                    width="20"
+                    height="20"
+                    color="brown"
+                  />
+                  <Typography>SuperMarket ($21)</Typography>
                 </Box>
               </Grid>
             </Grid>
+          </Box>
+        </Box>
+        <Box>
+          <Grid container>
+            <Grid size={6}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: 1,
+                  border: 1,
+                  borderLeft: 0,
+                  borderBottom: 0,
+                  borderStyle: "dotted",
+                  p: 3,
+                }}
+              >
+                <Typography variant="body1">Categories</Typography>
+                <Typography variant="h5" fontWeight="bold">
+                  9
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid size={6}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: 1,
+                  border: 1,
+                  borderRight: 0,
+                  borderBottom: 0,
+                  borderStyle: "dotted",
+                  p: 3,
+                }}
+              >
+                <Typography variant="body1">Categories</Typography>
+                <Typography variant="h5" fontWeight="bold">
+                  $18,765
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+      <Paper sx={{ mb: 2, borderRadius: 5 }} elevation={5}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            p: 3,
+          }}
+        >
+          <Box>
+            <Typography variant="h6">Recent transitions</Typography>{" "}
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            p: 3,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h6">view all </Typography> <ArrowRight />
           </Box>
         </Box>
       </Paper>
---
+++ src/pages/banking/components/incomeExpencePaper.tsx
@@ -0,0 +1,278 @@
+import { Icon } from "@iconify/react";
+import { Add, ArrowDownward, ArrowUpward, AttachMoney, InfoOutlineRounded } from "@mui/icons-material";
+import { Box, Button, Chip, Grid, Paper, Typography } from "@mui/material";
+import { LineChart, lineElementClasses, markElementClasses } from "@mui/x-charts";
+import { useState } from "react";
+
+
+const margin = { right: 24 };
+const incomeData = [5, 31, 33, 50, 99, 76, 72, 76, 89];
+const expenseData = [5, 31, 33, 50, 99, 76, 72, 76, 89];
+const xLabels = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep"];
+
+
+export default function IncomeExpenensePaper() {
+  const [isIncomeGraph, setIsIncomeGraph] = useState(true);
+  return (
+    <Paper sx={{ p: 3, mb: 2, borderRadius: 5 }} elevation={5}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body1">Total Balance </Typography>{" "}
+            <InfoOutlineRounded sx={{ fontSize: 16 }} />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <AttachMoney sx={{ fontWeight: "bold" }} />
+            <Typography variant="h5" fontWeight={"bold"}>
+              49,990
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "start",
+          }}
+        >
+          <Button
+            size="small"
+            variant="contained"
+            color="inherit"
+            href="/dashboard/tour/new"
+            disableElevation
+            startIcon={<ArrowUpward />}
+          >
+            Send
+          </Button>
+          <Button
+            size="small"
+            variant="contained"
+            color="inherit"
+            href="/dashboard/tour/new"
+            disableElevation
+            startIcon={<Add />}
+          >
+            Add Card
+          </Button>
+          <Button
+            size="small"
+            variant="contained"
+            color="inherit"
+            href="/dashboard/tour/new"
+            disableElevation
+            startIcon={<ArrowDownward />}
+          >
+            Request
+          </Button>
+        </Box>
+      </Box>
+      <Paper
+        sx={{
+          p: 3,
+          m: 1,
+          backgroundColor: "#f4f6f8ff",
+        }}
+      >
+        <Grid container>
+          <Grid size={6}>
+            <Button
+              color="inherit"
+              fullWidth
+              onClick={() => {
+                setIsIncomeGraph(true);
+              }}
+              sx={{
+                backgroundColor: isIncomeGraph ? "white" : "",
+                borderRadius: 5,
+              }}
+            >
+              <Box sx={{ display: "flex", flexDirection: "row", p: 3 }}>
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 2,
+                    right: 5,
+                  }}
+                >
+                  <Chip
+                    label="+8.2%"
+                    color="success"
+                    size="small"
+                    sx={{
+                      borderRadius: "8px",
+                      width: "80px",
+                    }}
+                    icon={<ArrowUpward />}
+                  />
+                </Box>
+                <Icon
+                  icon="eva:diagonal-arrow-left-down-fill"
+                  width="40"
+                  height="40"
+                />
+                <Box paddingLeft={3}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography variant="body1">Income </Typography>{" "}
+                    <InfoOutlineRounded sx={{ fontSize: 16 }} />
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <AttachMoney sx={{ fontWeight: "bold" }} />
+                    <Typography variant="h5" fontWeight={"bold"}>
+                      9,990
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Button>
+          </Grid>
+          <Grid size={6}>
+            <Button
+              color="inherit"
+              fullWidth
+              onClick={() => {
+                setIsIncomeGraph(false);
+              }}
+              sx={{
+                backgroundColor: isIncomeGraph ? "" : "white",
+                borderRadius: 5,
+              }}
+            >
+              <Box sx={{ display: "flex", flexDirection: "row", p: 3 }}>
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 1,
+                    right: 5,
+                  }}
+                >
+                  <Chip
+                    label="-6.6%"
+                    color="error"
+                    size="small"
+                    sx={{
+                      borderRadius: "8px",
+                      width: "80px",
+                    }}
+                    icon={<ArrowDownward />}
+                  />
+                </Box>
+                <Icon
+                  icon="eva:diagonal-arrow-right-up-fill"
+                  width="40"
+                  height="40"
+                />
+                <Box paddingLeft={3}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography variant="body1">Expenses </Typography>{" "}
+                    <InfoOutlineRounded sx={{ fontSize: 16 }} />
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <AttachMoney sx={{ fontWeight: "bold" }} />
+                    <Typography variant="h5" fontWeight={"bold"}>
+                      1989
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {isIncomeGraph ? (
+          <LineChart
+            height={300}
+            series={[{ data: incomeData, label: "income" }]}
+            xAxis={[{ scaleType: "point", data: xLabels }]}
+            yAxis={[{ width: 50 }]}
+            margin={margin}
+            sx={{
+              [`& .${lineElementClasses.root}`]: {
+                stroke: "#1cca56ff",
+                strokeWidth: 2,
+              },
+              [`& .${markElementClasses.root}`]: {
+                stroke: "#07f3c0ff",
+                r: 4, // Modify the circle radius
+                fill: "#fff",
+                strokeWidth: 2,
+              },
+            }}
+            disableAxisListener
+          />
+        ) : (
+          <LineChart
+            height={300}
+            series={[{ data: expenseData, label: "expense" }]}
+            xAxis={[{ scaleType: "point", data: xLabels }]}
+            yAxis={[{ width: 50 }]}
+            margin={margin}
+            sx={{
+              [`& .${lineElementClasses.root}`]: {
+                stroke: "#c2163bff",
+                strokeWidth: 2,
+              },
+              [`& .${markElementClasses.root}`]: {
+                stroke: "#f80021ff",
+                r: 4, // Modify the circle radius
+                fill: "#fff",
+                strokeWidth: 2,
+              },
+            }}
+            disableAxisListener
+          />
+        )}
+      </Box>
+    </Paper>
+  );
+}
